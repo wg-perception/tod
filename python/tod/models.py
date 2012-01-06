8b9a26db3c46b6f4bca28eb99468034a0c6a054e
Vincent Rabaud <vrabaud@willowgarage.com>
2012-01-06 13:53:32 -0800

    move TOD from object_recognition

---
+++ python/tod/models.py
@@ -0,0 +1,10 @@
+#!/usr/bin.env python
+"""
+Module containing function to find certain models in the DB
+"""
+
+def ObjectIdsToModelIds(object_ids, json_params):
+    """
+    Given object ids and a db, figure out the models that have the given parameters
+    """
+    
---
+++ test/test_tod_training.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+import ecto
+from tod.trainer import TODTrainingPipeline
+from object_recognition.common.utils.parser import ObjectRecognitionParser
+
+T = TODTrainingPipeline()
+
+parser = ObjectRecognitionParser()
+
+args = parser.parse_args()
+
+submethod = {'descriptor': {'type':'ORB'}}
+parameters = {'feature': {'type': 'ORB'}}
+T.incremental_model_builder(submethod=submethod, pipeline_params=parameters, args=args)
+T.post_processor(submethod=submethod, pipeline_params={'search': {'type': 'LSH'}}, args=args)

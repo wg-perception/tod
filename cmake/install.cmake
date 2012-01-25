9f3aa0e6fde51cb92cb80d1a43cdd1dc3976f78a
Vincent Rabaud <vrabaud@willowgarage.com>
2012-01-25 15:00:15 -0800

    no more include in here

--- cmake/install.cmake
+++ cmake/install.cmake
@@ -1,8 +1,4 @@
 set(prefix ${PROJECT_NAME}-${${PROJECT_NAME}_VERSION})
-install(DIRECTORY ${PROJECT_SOURCE_DIR}/include/
-  DESTINATION include/${prefix}
-  COMPONENT main
-  )
 
 #install the unix_install
 install(DIRECTORY ${CMAKE_BINARY_DIR}/share/

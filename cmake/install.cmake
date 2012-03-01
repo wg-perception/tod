0071de15793a6c7f0a0f73d2347a5397f0a9b3de
Vincent Rabaud <vrabaud@willowgarage.com>
2012-02-29 19:44:02 -0800

    no need for the install script anymore

--- cmake/install.cmake
+++
@@ -1,8 +0,0 @@
-set(prefix ${PROJECT_NAME}-${${PROJECT_NAME}_VERSION})
-
-#install the unix_install
-install(DIRECTORY ${CMAKE_BINARY_DIR}/share/
-  DESTINATION share/${prefix}
-  COMPONENT main
-  )
-

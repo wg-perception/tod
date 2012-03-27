b13945fcc21c01b300d8f0696f416e57806d7187
Vincent Rabaud <vrabaud@willowgarage.com>
2012-03-26 19:37:16 -0700

    use catkin for python

---
+++ setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+setup(name='Textured object detection',
+      version='1.0.0',
+      description='A pipeline for object recognition implementing a bag of feature approach',
+      packages=['tod'],
+      package_dir={'':'python'}
+)

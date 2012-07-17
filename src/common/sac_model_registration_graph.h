ab0e2c5097c86c89a2c25c0b2a353a4241878cea
Vincent Rabaud <vrabaud@willowgarage.com>
2012-07-17 13:14:52 -0700

    fix a bug in the sub-graph building to improve accuracy. Also add speedups

--- src/common/sac_model_registration_graph.h
+++ src/common/sac_model_registration_graph.h
@@ -109,9 +109,9 @@ namespace tod
         int sample = valid_samples[rand() % valid_samples.size()];
         IndexVector new_valid_samples(valid_samples.size());
         IndexVector::iterator end = std::set_intersection(valid_samples.begin(), valid_samples.end(),
-                                                               sample_adjacency_.neighbors(sample).begin(),
-                                                               sample_adjacency_.neighbors(sample).end(),
-                                                               new_valid_samples.begin());
+                                                          sample_adjacency_.neighbors(sample).begin(),
+                                                          sample_adjacency_.neighbors(sample).end(),
+                                                          new_valid_samples.begin());
         new_valid_samples.resize(end - new_valid_samples.begin());
         if (drawIndexSampleHelper(new_valid_samples, n_samples - 1))
         {
@@ -198,59 +198,71 @@ namespace tod
       inliers.resize(nr_p);
 
       // If that set is not bigger than the best so far, no need to refine it
-      unsigned int minimal_size = 8;
-      if ((inliers.size() <= best_inlier_number_) && (inliers.size() < minimal_size))
+      size_t minimal_size = std::min(best_inlier_number_, size_t(7));
+      if (inliers.size() <= minimal_size)
         return;
-      std::sort(inliers.begin(), inliers.end());
 
       // We are now going to check that we can come up with a big enough sample adjacency clique
       // As this rarely happens, first make sure that some inliers have enough neighbors
-      size_t max_possible_clique = 0;
-      std::vector<unsigned int> neighbors(inliers.size());
-      for (unsigned int j = 0; j < inliers.size() - 1; ++j)
+      std::vector<unsigned int> filtered_inliers;
+      filtered_inliers.reserve(inliers.size());
+      for (unsigned int j = 0; j < inliers.size(); ++j)
+        if (sample_adjacency_.neighbors(inliers[j]).size() >= minimal_size)
+          filtered_inliers.push_back(inliers[j]);
+      if (filtered_inliers.size() <= minimal_size)
       {
-        max_possible_clique = std::max(
-            size_t(
-                std::set_intersection(sample_adjacency_.neighbors(j).begin(), sample_adjacency_.neighbors(j).end(),
-                                      inliers.begin(), inliers.end(), neighbors.begin())
-                - neighbors.begin()),
-            max_possible_clique);
-        if ((max_possible_clique >= minimal_size) && (max_possible_clique > best_inlier_number_))
-          break;
+        inliers.clear();
+        return;
       }
-      if ((max_possible_clique < minimal_size) || (max_possible_clique <= best_inlier_number_))
+      std::sort(filtered_inliers.begin(), filtered_inliers.end());
+
+      // Then make sure that those inliers have enough neighbors within the inliers themselves
+      size_t max_possible_clique = 0;
+      std::vector<unsigned int> neighbors(filtered_inliers.size());
+      BOOST_FOREACH(Index inlier, filtered_inliers){
+      max_possible_clique =
+      size_t(
+          std::set_intersection(sample_adjacency_.neighbors(inlier).begin(),
+              sample_adjacency_.neighbors(inlier).end(), filtered_inliers.begin(),
+              filtered_inliers.end(), neighbors.begin())
+          - neighbors.begin());
+      if (max_possible_clique > minimal_size)
+      break;
+    }
+      if (max_possible_clique <= minimal_size)
       {
         inliers.clear();
         return;
       }
 
-      // look for big enough cliques
+      // Build the adjacency graph
       std::map<unsigned int, unsigned int> map_index_to_graph_index;
-      for (unsigned int j = 0; j < inliers.size(); ++j)
-        map_index_to_graph_index[inliers[j]] = j;
+      for (unsigned int j = 0; j < filtered_inliers.size(); ++j)
+        map_index_to_graph_index[filtered_inliers[j]] = j;
 
-      maximum_clique::Graph graph(inliers.size());
-      for (unsigned int j = 0; j < inliers.size() - 1; ++j)
+      maximum_clique::Graph graph(filtered_inliers.size());
+      for (unsigned int j = 0; j < filtered_inliers.size() - 1; ++j)
       {
-        neighbors.resize(inliers.size());
-        std::vector<unsigned int>::iterator end = std::set_intersection(sample_adjacency_.neighbors(j).begin(),
-                                                                        sample_adjacency_.neighbors(j).end(),
-                                                                        inliers.begin() + j + 1, inliers.end(),
-                                                                        neighbors.begin());
+        neighbors.resize(filtered_inliers.size());
+        std::vector<unsigned int>::iterator end = std::set_intersection(
+            sample_adjacency_.neighbors(filtered_inliers[j]).begin(),
+            sample_adjacency_.neighbors(filtered_inliers[j]).end(), filtered_inliers.begin() + j + 1,
+            filtered_inliers.end(), neighbors.begin());
         neighbors.resize(end - neighbors.begin());
 
         BOOST_FOREACH(unsigned int neighbor, neighbors)graph.AddEdgeSorted(j, map_index_to_graph_index[neighbor]);
       }
 
-    // If we cannot even find enough points well distributed in the sample, stop here
+      // If we cannot even find enough points well distributed in the sample, stop here
       std::vector<unsigned int> vertices;
       graph.FindClique(vertices, minimal_size);
-      if (vertices.size() < minimal_size)
+      if (vertices.size() <= minimal_size)
       {
         inliers.clear();
         return;
       }
 
+      std::sort(inliers.begin(), inliers.end());
       best_inlier_number_ = std::max(inliers.size(), best_inlier_number_);
     }
 
@@ -266,9 +278,9 @@ namespace tod
 
       // Make sure the sample do verify the transform
       /*BOOST_FOREACH(Index sample, samples){
-      if (distSq(R*query_points_[sample] + T, training_points_[sample])>threshold_*threshold_)
-      return false;
-    }*/
+       if (distSq(R*query_points_[sample] + T, training_points_[sample])>threshold_*threshold_)
+       return false;
+       }*/
 
       return true;
     }
@@ -296,25 +308,23 @@ namespace tod
       cv::Vec3f centroid_training(0, 0, 0), centroid_query(0, 0, 0);
 
       // Estimate the centroids of source, target
-      BOOST_FOREACH(Index index, indices_src)
-      {
-        centroid_training += training_points_[index];
-        centroid_query += query_points_[index];
-      }
+      BOOST_FOREACH(Index index, indices_src){
+      centroid_training += training_points_[index];
+      centroid_query += query_points_[index];
+    }
       centroid_training /= float(indices_src.size());
       centroid_query /= float(indices_src.size());
 
       // Subtract the centroids from source, target
-      cv::Mat_<cv::Vec3f> sub_training(indices_src.size(),1), sub_query(indices_src.size(),1);
+      cv::Mat_<cv::Vec3f> sub_training(indices_src.size(), 1), sub_query(indices_src.size(), 1);
       unsigned int i = 0;
-      BOOST_FOREACH(Index index, indices_src)
-      {
-        sub_training(i) = training_points_[index] - centroid_training;
-        sub_query(i) = query_points_[index] - centroid_query;
-        ++i;
-      }
+      BOOST_FOREACH(Index index, indices_src){
+      sub_training(i) = training_points_[index] - centroid_training;
+      sub_query(i) = query_points_[index] - centroid_query;
+      ++i;
+    }
 
-      // Assemble the correlation matrix
+    // Assemble the correlation matrix
       cv::Mat H = sub_training.reshape(1, indices_src.size()).t() * sub_query.reshape(1, indices_src.size());
 
       // Compute the Singular Value Decomposition
@@ -325,7 +335,7 @@ namespace tod
       if (cv::determinant(svd.u) * cv::determinant(vt) < 0)
       {
         for (int x = 0; x < 3; ++x)
-        vt(2, x) *= -1;
+          vt(2, x) *= -1;
       }
 
       R_in = cv::Mat(svd.u * vt);
